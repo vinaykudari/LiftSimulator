2c13853601a34abc0535d12fcc1c09642cb6a299
Vinay Kudari <svvk.20140513@btech.nitdgp.ac.in>
2019-05-27 23:32:01 +0530

    kv file

--- app.py
+++ app.py
@@ -2,20 +2,13 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
 
-class Layout(GridLayout):
-
-    def __init__(self, **kwargs):
-        super(Layout, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text="Name : "))
-        self.name = TextInput(multiline=False)
-        self.add_widget(self.name)
-        self.name1 = TextInput(multiline=False)
-        self.add_widget(self.name1)
+class Layout(Widget):
+    pass
 
 class LiftSimulator(App):
-
+    
     def build(self):
         return Layout()
 
