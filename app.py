f37e5f6ccbcf00c875d7521117fafa3dc44910db
Vinay Kudari <svvk.20140513@btech.nitdgp.ac.in>
2019-06-01 20:56:23 +0530

    Started with pygame

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from kivy.app import App 
-from kivy.uix.floatlayout import FloatLayout
+import pygame
 
-class LiftSimulator(App):
+class Lift:
+    def __init__(self, screen, floors, no_of_lifts):
+        floor = []
+        w, h = pygame.display.get_surface().get_size()
+        for i in range(floors):
+            pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(w/2, h/10+i*50, 40, 15))
+        pygame.display.flip()
 
-    def build(self):
-        return FloatLayout()
+        while True:
+            event = pygame.event.wait()
 
-if __name__ == "__main__":
-    LiftSimulator().run()
+            if event.type == pygame.QUIT:
+                break
+
+pygame.init()
+screen = pygame.display.set_mode((1000, 500))
+lift = Lift(screen, floors=8, no_of_lifts=6)
