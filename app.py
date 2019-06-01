fc020a242f9f3a19027ba5f3ce1389cbc2a40573
Vinay Kudari <svvk.20140513@btech.nitdgp.ac.in>
2019-06-01 12:57:26 +0530

    floatlayout testing

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
 from kivy.app import App 
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
-
-class LayoutView(Widget):
-    name = ObjectProperty(None)
-    email = ObjectProperty(None)
-
-    def button_press(self):
-        print("Name : ", self.name.text, " Email : ", self.email.text)
+from kivy.uix.floatlayout import FloatLayout
 
 class LiftSimulator(App):
 
     def build(self):
-        return LayoutView()
+        return FloatLayout()
 
 if __name__ == "__main__":
     LiftSimulator().run()
