21f69177aa06c5445bd4d10a060ce00df0c2b539
Vinay Kudari <svvk.20140513@btech.nitdgp.ac.in>
2019-06-02 12:34:07 +0530

    completed basic structure

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ class Lift:
     def __init__(self, screen, floors, no_of_lifts):
         floor = []
         w, h = pygame.display.get_surface().get_size()
-        for i in range(floors):
-            pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(w/2, h/10+i*50, 40, 15))
+        for no in range(no_of_lifts):
+            for floor in range(floors):
+                pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(w/10+no*100, h/10+floor*50, 40, 15))
         pygame.display.flip()
 
         while True:
@@ -16,4 +17,4 @@ class Lift:
 
 pygame.init()
 screen = pygame.display.set_mode((1000, 500))
-lift = Lift(screen, floors=8, no_of_lifts=6)
+lift = Lift(screen, floors=4, no_of_lifts=3)
