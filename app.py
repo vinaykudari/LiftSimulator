be000663b714eb99d4bba35033c2dc629a820202
Vinay Kudari <svvk.20140513@btech.nitdgp.ac.in>
2019-06-05 23:54:53 +0530

    added blocks

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import pygame
 
 class LiftSimulator:
 
-    def __init__(self, floors, no_of_lifts):
+    def __init__(self, floors, lifts):
         pygame.init()
         self.floors = floors
-        self.no_of_lifts = no_of_lifts
+        self.lifts = lifts
         self.screen = pygame.display.set_mode((1000, 500))
     
     def run(self):
@@ -18,10 +18,19 @@ class LiftSimulator:
     
     def generate_structure(self):
         floor = []
-        w, h = pygame.display.get_surface().get_size()
-        for no in range(self.no_of_lifts):
-            for floor in range(self.floors):
-                pygame.draw.rect(self.screen, (255, 0, 0), pygame.Rect(w/10+no*100, h/10+floor*50, 40, 15))
+        background = pygame.Surface(self.screen.get_size())
+        background.fill((255, 255, 255))
+        for no in range(self.lifts):
+            block = pygame.Surface((60, (self.floors*2)*16))
+            block.fill((240, 240, 240))
+            w, h = block.get_size()
+            for lift in range(self.lifts):
+                for floor in range(self.floors):
+                    pygame.draw.rect(block, (255, 0, 0), pygame.Rect(10, 15+floor*30, 40, 15))
+                background.blit(block, (50+75*lift, 100))
+        background = background.convert()
+        self.screen.blit(background, (0,0))
         pygame.display.flip()
+            
 
-lift = LiftSimulator(floors=5, no_of_lifts=3).run()
+lift = LiftSimulator(floors=5, lifts=3).run()
